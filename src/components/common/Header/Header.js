28c1a285e047ca70f7a61a685256fafc15b2a2c2
sujk912 <aa991122aa@gmail.com>
2022-05-28 03:11:34 +0900

    Create Header Navigation Component and apply it into App.js

---
+++ src/components/common/Header/Header.js
@@ -0,0 +1,38 @@
+import Button from './../Button/Button';
+import styled from 'styled-components/native';
+import theme from './../../../theme';
+
+export const LogoTitle = () =>{
+    return (
+        <Styled.leftContainer>
+            <Styled.logoText>MOIN</Styled.logoText>
+        </Styled.leftContainer>
+    );
+}
+
+export const HeaderRight = () =>{
+    return (
+    <Styled.rightContainer>
+        <Button type={'Square'} title={'안녕핫요'} onPress={()=>console.log("TEST")} / >
+        <Button type={'Square'} title={`얄로`} onPress={()=>console.log("TT")} / >
+    </ Styled.rightContainer>
+    );
+}
+
+const Styled = {
+    logoText: styled.Text`
+        font-weight: bold;
+        font-size: ${theme.fontSize.xxl}px;
+    `,
+    leftContainer: styled.View`
+        padding-left: 24px;
+    `,
+    rightContainer: styled.View`
+        display: flex;
+        flex-direction: row;
+        gap: 10px;
+        justify-align: right;
+        align-items: center;
+        padding-right: 24px;
+    `,
+}
