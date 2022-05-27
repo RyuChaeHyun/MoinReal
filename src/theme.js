22b3ab29110f7e3f5620416a21464a7492f169eb
sujk912 <aa991122aa@gmail.com>
2022-05-28 03:08:23 +0900

    Change Style Theme(Add fontSize)

--- src/theme.js
+++ src/theme.js
@@ -1,7 +1,35 @@
-export const colors = {
+import normalize from "../lib/normalize";
+
+const colors = {
     primary: '#F9EB6C',
     dark: '#000000',
+    darkgray: `#373737`,
     white: '#FFFFFF',
     gray: '#E6E6E6',
     beige: '#FFFDEB',
 };
+
+const fontSize = {
+    xs: 12,
+    sm: 14,
+    md: 16,
+    lg: 18,
+    xl: 20,
+    xxl: 24,
+    xxxl: 32,
+    xxxl: 48,
+    // xs: normalize(12),
+    // sm: normalize(14),
+    // md: normalize(16),
+    // lg: normalize(18),
+    // xl: normalize(20),
+    // xxl: normalize(24),
+    // xxxl: normalize(32),
+    // xxxl: normalize(48),
+}
+
+const theme = {
+    colors,
+    fontSize: fontSize,
+}
+export default theme;
