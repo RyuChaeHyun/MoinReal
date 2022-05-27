f0ae932482a83e2f0daa0d5ef5d5331efd466a1a
sujk912 <aa991122aa@gmail.com>
2022-05-28 03:12:27 +0900

    Create Signup View (TODO: Add saving userInfo Logic)

--- src/components/auth/signup.js
+++ src/components/auth/signup.js
@@ -1,56 +1,97 @@
 import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
-import { useState } from "react";
+import React, { useRef, useState } from "react";
 import { Alert, Text, View } from "react-native";
 import { TextInput, TouchableOpacity } from "react-native-gesture-handler";
+import styled from 'styled-components/native';
+import Input from "../common/Input/Input";
+import TitleText from "../common/TitleText/TitleText";
+import theme from './../../theme';
+import Button from './../common/Button/Button';
+import FormScrollView from './../common/FormScrollView/FormScrollView';
 
 const Signup = () => {
-  const [ username, setUsername ] = useState('');
   const [ email, setEmail ] = useState('');
   const [ password, setPassword ] = useState('');
-  const auth = getAuth();
-  createUserWithEmailAndPassword(auth, email, password)
-  .then((userCredential) => {
-    // Signed in
-    const user = userCredential.user;
-    // ...
-  })
-  .catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    // ..
-  });
+  const [ confirmPassword, setConfirmPassword ] = useState('');
+  const [ name, setName ] = useState('');
+  const [ username, setUsername ] = useState('');
+  const [ village, setVillage ] = useState('');
+  const [ phone, setPhone ] = useState('');
+  
+  const registration = () =>{
+    const auth = getAuth();
+    createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      // Signed in
+      const user = userCredential.user;
+      // ...
+      console.log(user);
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      Alert.alert(errorMessage);
+      console.log(errorMessage);
+    });
+  }
+
   return (
-    <View>
-   <View> 
-        <Text> Registration </Text> 
-    </View>
-    <TextInput
-      underlineColorAndroid = "transparent"
-      placeholder = "Enter Your Name"
-      placeholderTextColor = "#9a73ef"
-      onChangeText = {text => setUsername(text)}/>
-
-    <TextInput 
-      underlineColorAndroid = "transparent"
-      placeholder = "Enter Your Email"
-      placeholderTextColor = "#9a73ef"
-      autoCapitalize = "none"
-      onChangeText = {text=>setEmail(text)}/>
-
-    <TextInput 
-      underlineColorAndroid = "transparent"
-      placeholder = "Enter Your Password"
-      placeholderTextColor = "#9a73ef"
-      autoCapitalize = "none"
-      secureTextEntry = "true"
-      onChangeText = {text => setPassword(text)}/>
-
-    <TouchableOpacity>
-      <Text> Create Account </Text>
-    </TouchableOpacity>
-
-    </View>
+    <FormScrollView title={'시작하기'} type={'Round'} onPress={registration}>
+      <Styled.innerContainer>
+        <TitleText title={'가입정보'} />
+        <Input 
+          title={'이메일'} 
+          placeholder={'이메일을 입력해주세요'} 
+          onChangeSetText={setEmail} 
+        />
+        <Input 
+          title={'비밀번호'} 
+          placeholder={'비밀번호를 입력해주세요'} 
+          onChangeSetText={setPassword} 
+          secureTextEntry = {true}
+        />
+        <Input 
+          title={'비밀번호 확인'} 
+          placeholder={'비밀번호를 입력해주세요'} 
+          onChangeSetText={setConfirmPassword} 
+          secureTextEntry = {true}
+        />
+      </Styled.innerContainer>
+      <Styled.innerContainer>
+        <TitleText title={'필수정보'}/>
+        <Input 
+          title={'이름'} 
+          placeholder={'본명을 입력해주세요'} 
+          onChangeSetText={setName} 
+        />
+        <Input 
+          title={'닉네임'} 
+          placeholder={'사용할 닉네임을 입력해주세요'} 
+          onChangeSetText={setUsername} 
+        />
+        <Input 
+          title={'동네인증'} 
+          placeholder={'현재 거주지를 입력해주세요'} 
+          onChangeSetText={setVillage} 
+        />
+        <Input 
+          title={'연락처'} 
+          placeholder={'전화번호를 입력해주세요'} 
+          onChangeSetText={setPhone} 
+          keyboardType='numeric'
+        />
+      </Styled.innerContainer>
+    </FormScrollView>
   );
+}
+
+const Styled = {
+  container: styled.SafeAreaView`
+
+  `,
+  innerContainer: styled.View`
+    padding-bottom: ${theme.fontSize.xl}px;
+  `,
 }
 
 export default Signup;
