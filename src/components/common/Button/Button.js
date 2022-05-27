a343096e49977177f823bd50e401e775f89e3a11
sujk912 <aa991122aa@gmail.com>
2022-05-28 03:07:28 +0900

    Create Button Component

---
+++ src/components/common/Button/Button.js
@@ -0,0 +1,48 @@
+import styled from 'styled-components/native';
+import theme from './../../../theme';
+
+// type은 Square 혹은 Round만 가능하다.
+const Button = ({type, title, children, ...props}) => {
+    if(type==='Square') {
+        return (
+        <Styled.squareButtonContainer {...props}>
+            <Styled.text>
+                {title}
+            </Styled.text>
+        </Styled.squareButtonContainer>
+        );
+    }
+    else if(type==='Round') {
+        return (
+            <Styled.roundButtonContainer {...props}>
+                <Styled.text>
+                    {title}
+                </Styled.text>
+            </Styled.roundButtonContainer>
+        );
+    }
+}
+// Style
+const Styled = {
+    roundButtonContainer: styled.TouchableOpacity`
+        background-color: ${theme.colors.primary};
+        justify-content: center;
+        align-items: center;
+        border-radius: 150px;
+        height: 48px;
+        `,
+    squareButtonContainer: styled.TouchableOpacity`
+        background-color: ${theme.colors.primary};
+        justify-content: center;
+        align-items: center;
+        border-radius: 5px;
+        width:100%;
+    `,
+    text: styled.Text`
+        font-weight: bold;
+        font-size: ${theme.fontSize.md}px;
+    `,
+}
+
+// Export
+export default Button;
