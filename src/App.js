af8a3acefb05060a9b41c3d88905cbd1f229b8c4
RyuChaeHyun <lch010201@naver.com>
2022-05-30 20:46:06 +0900

    marketlist_Create

--- src/App.js
+++ src/App.js
@@ -4,13 +4,12 @@ import config from '../setting/firebase.json';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import SharingInfoList from './components/sharingInfo/SharingInfoList';
-import MarketList from './components/Market/MarketList';
 import JobFindingList from './components/JobFinding/JobFindingList';
 import JobOpeningList from './components/JobOpening/JobOpeningList';
+import MarketList from './components/Market/MarketList';
 import { colors } from './theme';
 import Signin from './components/auth/signin';
 import Signup from './components/auth/signup';
-import SharingInfoUpdate from './components/sharingInfo/SharingInfoUpdate';
 
 export default function App() {
   firebase.initializeApp(config);
@@ -30,11 +29,11 @@ export default function App() {
                     fontWeight: 'bold',
                     fontSize: '30px',
                   },
-                  initialRouteName: 'SharingInfo'
+                  initialRouterName:'SharingInfo',
                 }}>
                   <Stack.Screen name="Singin" component={Signin} options={{ headerShown: false }} />
                   <Stack.Screen name="Signup" component={Signup} options={{ headerShown: false }}/>
-                  <Stack.Screen name="SharingInfo" component={SharingInfoUpdate} />
+                  <Stack.Screen name="SharingInfo" component={MarketList} />
                   <Stack.Screen name="Market" component={MarketList} />
                   <Stack.Screen name="JobFinding" component={JobFindingList} />
                   <Stack.Screen name="JobOpening" component={JobOpeningList} />
