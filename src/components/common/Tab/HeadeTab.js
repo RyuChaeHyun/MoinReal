1465f6f5bb4af30a5f8b51e3b4c3a0d47e7c5e38
sujk912 <aa991122aa@gmail.com>
2022-06-16 08:26:14 +0900

    Fix HeaderTab Error

--- src/components/common/Tab/HeadeTab.js
+++ src/components/common/Tab/HeadeTab.js
@@ -1,28 +1,61 @@
 import React from 'react';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { HeaderRight, LogoTitle } from '../Header/Header';
-import TabNavigator from './TabNavigator';
+import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs';
+import JobOpeningList from '../../../pages/JobOpening/JobOpeningList';
+import JobFindingList from '../../../pages/JobFinding/JobFindingList';
+import SharingInfoList from '../../../pages/SharingInfo/SharingInfoList';
 import theme from '../../../theme';
-const Stack = createNativeStackNavigator();  
+import MarketList from './../../../pages/Market/MarketList';
 
-const HeaderTab = () => {
+const Tab = createMaterialTopTabNavigator();
+
+function HeaderTab(){
     return(
-        <Stack.Navigator
-            screenOptions={{
-                title: '',
-                headerTintColor: theme.colors.dark,
-                headerStyle: {
-                backgroundColor: theme.colors.primary,
+        <Tab.Navigator 
+            initialRouteName='SharingInfoList'
+            tabBarOptions={{
+                indicatorStyle: {
+                    backgroundColor: theme.colors.labelprimary,
                 },
-            headerTitleStyle: {
-                fontWeight: 'bold',
-                fontSize: theme.fontSize.lg,
-            },
-            headerLeft: ()=><LogoTitle />,
-            headerRight:  ()=><HeaderRight/>,
-            }}>
-            <Stack.Screen name="HeaderTab" component={TabNavigator}/>
-        </Stack.Navigator>
-    );
-};
- export default HeaderTab;
+                activeTintColor:theme.colors.labelprimary,
+                inactiveTintColor: theme.colors.dark,
+                labelStyle:{
+                    fontSize:17,
+                    fontWeight:"bolder",
+                },
+                style:{
+                    height:50,
+                }
+             }}
+        >
+            <Tab.Screen
+                name="SharingInfo"
+                component={SharingInfoList}
+                options={{
+                    tabBarLabel:'정보공유'
+                }}
+             />
+             <Tab.Screen
+                name="JobOpening"
+                component={JobOpeningList}
+                options={{
+                    tabBarLabel:'구인'
+                }}
+             />
+             <Tab.Screen
+                name="JobFinding"
+                component={JobFindingList}
+                options={{
+                    tabBarLabel:'구직'
+                }}
+             />
+             <Tab.Screen
+                name="Market"
+                component={MarketList}
+                options={{
+                    tabBarLabel:'중고거래'
+                }}
+             />
+        </Tab.Navigator>
+    )
+}
+export default HeaderTab;
